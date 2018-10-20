adc877773b3fb2c91ff9dab1dda96b8b8da0662b
EC2 Default User <ec2-user@ip-172-31-5-61.ec2.internal>
2018-10-20 01:04:54 +0000

    Segundo Commit

---
+++ cuboSemantico.py
@@ -0,0 +1,128 @@
+#Gala Stefanía Ramos Islas a00817135
+#Luis Adolfo Diaz Torres   A01280862
+
+cuboSemantico = {
+    "int": {
+        "int": {
+            "+":"int",
+            "-":"int",
+            "*":"int",
+             "/":"int",
+             "=":"int",
+             "==":"bool",
+             "!=":"bool",
+             ">":"bool",
+             "<":"bool",
+             ">=":"bool",
+             "<=":"bool"
+        },
+        "float": {
+            "+":"float",
+            "-":"float",
+            "*":"float",
+             "/":"float",
+             "=":"float",
+             "==":"bool",
+             "!=":"bool",
+             ">":"bool",
+             "<":"bool",
+             ">=":"bool",
+             "<=":"bool"
+        },
+        "string": {
+            "+":"Error",
+            "-":"Error",
+            "*":"Error",
+             "/":"Error",
+             "=":"Error",
+             "==":"Error",
+             "!=":"Error",
+             ">":"Error",
+             "<":"Error",
+             ">=":"Error",
+             "<=":"Error"
+        }
+    },
+    "float" : {
+        "int": {
+            "+":"float",
+            "-":"float",
+            "*":"float",
+             "/":"float",
+             "=":"float",
+             "==":"bool",
+             "!=":"bool",
+             ">":"bool",
+             "<":"bool",
+             ">=":"bool",
+             "<=":"bool"
+        },
+        "float": {
+            "+":"float",
+            "-":"float",
+            "*":"float",
+             "/":"float",
+             "=":"float",
+             "==":"bool",
+             "!=":"bool",
+             ">":"bool",
+             "<":"bool",
+             ">=":"bool",
+             "<=":"bool"
+        },
+        "string": {
+            "+":"Error",
+            "-":"Error",
+            "*":"Error",
+             "/":"Error",
+             "=":"Error",
+             "==":"Error",
+             "!=":"Error",
+             ">":"Error",
+             "<":"Error",
+             ">=":"Error",
+             "<=":"Error"
+        }
+    },
+    "string" : {
+        "int": {
+            "+":"Error",
+            "-":"Error",
+            "*":"Error",
+             "/":"Error",
+             "=":"Error",
+             "==":"Error",
+             "!=":"Error",
+             ">":"Error",
+             "<":"Error",
+             ">=":"Error",
+             "<=":"Error"
+        },
+        "float": {
+            "+":"Error",
+            "-":"Error",
+            "*":"Error",
+             "/":"Error",
+             "=":"Error",
+             "==":"Error",
+             "!=":"Error",
+             ">":"Error",
+             "<":"Error",
+             ">=":"Error",
+             "<=":"Error"
+        },
+        "string": {
+            "+":"Error",
+            "-":"Error",
+            "*":"Error",
+             "/":"Error",
+             "=":"string",
+             "==":"bool",
+             "!=":"bool",
+             ">":"Error",
+             "<":"Error",
+             ">=":"Error",
+             "<=":"Error"
+        }
+    }
+}
